f5245313c9b40fedef1cb9139ece9677721d34e0
Ben Birdsall & Kelly Becker <student@epicodus.com>
2014-02-20 14:52:38 -0800

    loads cryptosquare files

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,15 @@
-var fibonacci = function(number) {
-  if (isBadInput(number)) {
-    return "Cannot compute Fibonacci sequence for non-positive positions, non-integers, or non-numbers!";
-  } else {
-    return sequence(number);
-  }
+var cryptosquare = function(string) {
+  
 };
-
-var isBadInput = function(number) {
-  return (number < 1 || isNaN(number) || number % 1 !== 0);
-};
-
-var sequence = function(index){
-  var fibonacciSequence = [0,1];
-  for (var i = 1; i <= index - 2; i++) {
-    fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i]);
-  }
-  return fibonacciSequence[index - 1];
-};
-
-// var sequence = function(index) {
-//   if (index === 1) {
-//     return 0;
-//   } else if (index === 2) {
-//     return 1;
-//   } else {
-//     return sequence(index - 1) + sequence(index - 2); 
-//   }
-// };
 
 $(document).ready(function() {
-  $('#fibonacciNautilus').hide();
-  $('form#fibonacci').submit(function(event) {
+  $('#cryptosquareImage').hide();
+  $('form#cryptosquare').submit(function(event) {
     event.preventDefault();
     var numberInput = $('input#inputNumber').val();
     var result = fibonacci(numberInput);
     $('#result').text(result);
-    $('#fibonacciNautilus').show();
+    $('#cryptosquareImage').show();
     this.reset();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,25 +1,5 @@
-describe('fibonacci',function() {
+describe('cryptosquare',function() {
   it('should return 0 for 1', function() {
-   fibonacci(1).should.equal(0);
-  });
-
-  it('should return error message for non-numbers', function() {
-    fibonacci("hi").should.equal("Cannot compute Fibonacci sequence for non-positive positions, non-integers, or non-numbers!");
-  });
-
-  it('should return error message for negative numbers', function() {
-    fibonacci(-1).should.equal("Cannot compute Fibonacci sequence for non-positive positions, non-integers, or non-numbers!");
-  });
-
-  it('should return error message for non-integers', function() {
-    fibonacci(3.5).should.equal("Cannot compute Fibonacci sequence for non-positive positions, non-integers, or non-numbers!");
-  });
-
-  it('should return 1 for 2', function() {
-   fibonacci(2).should.equal(1);
-  });
-
-  it('should return 21 for 9', function() {
-   fibonacci(9).should.equal(21);
+   cryptosquare(1).should.equal(0);
   });
 });

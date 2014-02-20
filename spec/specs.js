968167a24aaad4632d3483d0b412771e2e49ef78
Ben Birdsall & Kelly Becker <student@epicodus.com>
2014-02-20 11:52:21 -0800

    creates initial files

--- spec/specs.js
+++ spec/specs.js
@@ -1,26 +1,6 @@
-describe('factorial',function() {
+describe('fibonacci',function() {
   it('should return 1 for 1', function() {
-    factorial(1).should.equal(1);
-  });
-  
-  it('should return 1 for 0', function() {
-    factorial(0).should.equal(1);
-  });
-
-  it('should return 6 for 3', function() {
-    factorial(3).should.equal(6);
-  });
-
-  it('should return error message for negative number', function() {
-    factorial(-3).should.equal("Cannot compute factorial for negative numbers, non-integers, or non-numbers!");
-  });
-
-  it('should return error message for non-number', function() {
-    factorial("Problem!").should.equal("Cannot compute factorial for negative numbers, non-integers, or non-numbers!");
-  });
-
-  it('should return error message for non-integers', function() {
-    factorial(3.5).should.equal("Cannot compute factorial for negative numbers, non-integers, or non-numbers!");
+   fibonacci(1).should.equal(1);
   });
 
 });

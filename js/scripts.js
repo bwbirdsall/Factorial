807241c976960d5dc2451593f85c4f01dfca4516
Ben Birdsall & Kelly Becker <student@epicodus.com>
2014-02-20 16:19:51 -0800

    we did everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,55 @@
 var cryptosquare = function(string) {
-  
+  string = string.toLowerCase();
+
+  string = removeSpacesAndPunc(string);
+
+  var squareNoColumns = boxSize(string);
+
+  string = encode(string, squareNoColumns);
+
+  string = insertSpaces(string);
+
+  return string;
 };
 
+var removeSpacesAndPunc = function(string) {
+  var regEx = /\W/g;
+  return string = string.replace(regEx, "");
+};
+
+var boxSize = function(string) {
+  return Math.ceil(Math.sqrt(string.length));
+};
+
+var encode = function(string, columns) {
+  var box = [];
+  var encodedString = "";
+
+  while (string.length > 0) {
+    box.push(string.slice(0,columns));
+    string = string.slice(columns);
+  }
+
+  for(var i = 0; i < columns; i++){
+    for(var j = 0; j < box.length; j++) {
+      encodedString += box[j].charAt(i);
+    }
+  }
+  return encodedString;
+};
+
+var insertSpaces = function(string) {
+  var regEx = /(\w{5})/g;
+  return string = string.replace(regEx, "$1 ");
+};
+
+
 $(document).ready(function() {
-  $('#cryptosquareImage').hide();
   $('form#cryptosquare').submit(function(event) {
     event.preventDefault();
-    var numberInput = $('input#inputNumber').val();
-    var result = fibonacci(numberInput);
+    var string = $('input#inputString').val();
+    var result = cryptosquare(string);
     $('#result').text(result);
-    $('#cryptosquareImage').show();
     this.reset();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,29 @@
 describe('cryptosquare',function() {
-  it('should return 0 for 1', function() {
-   cryptosquare(1).should.equal(0);
+  it('should return a valid cryptosquare', function() {
+   cryptosquare("aaaa").should.equal("aaaa");
+  });
+});
+
+describe('removeSpacesAndPunc',function() {
+  it('should return "nospaces" for "no spaces!"', function() {
+   removeSpacesAndPunc("no spaces!").should.equal("nospaces");
+  });
+});
+
+describe('boxSize',function() {
+  it('should return proper column number for given string\'s length', function() {
+   boxSize("nospacesnospacesa").should.equal(5);
+  });
+});
+
+describe('encode',function() {
+  it('should return proper box spacing for a string and column number', function() {
+   encode("nospacesnospacesa", 5).should.equal("ncssoepassapncaoe");
+  });
+});
+
+describe('insertSpaces',function() {
+  it('should place spaces in a string every five characters', function() {
+   insertSpaces("nospacesnospacesa").should.equal("nospa cesno space sa");
   });
 });
